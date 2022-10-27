f266e9459505a4187379331ab3d30c6bd0dded36
Subhanu Sankar Roy <71728589+SubhanuSRoy@users.noreply.github.com>
2022-10-27 15:15:23 +0530

    added allowed hosts

--- infirmary/infirmary/settings.py
+++ infirmary/infirmary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f)_&gk$wcf(zhrf8n*+9yn&jf0b=vbj+c8_s@aadf!nh+y(np0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
